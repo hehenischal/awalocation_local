cd99cf4f976142a645d61d33b574bd1ebe2ea1d8
hehenischal <nischallc56@gmail.com>
2023-07-23 21:46:24 +0545

    changed the DEBUG mode

--- awalocation_local/settings.py
+++ awalocation_local/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6&=4t%f#%z39ndnxth9qjj_8j+5e!nxmfk8o!d-ee9v%o%s0+@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*' ]
 
